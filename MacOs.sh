f9253c763cfbebcf70281f128a087025c6006f48
DereckAn <dereckangeles15071998@gmail.com>
2024-07-10 02:23:18 -0600

    updatins scripts

--- MacOs.sh
+++ MacOs.sh
@@ -35,10 +35,13 @@ fi
 # Cambiar al directorio de descargas
 cd ~/Downloads
 
-# Clonar la carpeta específica del repositorio de GitHub
-git clone --depth 1 --filter=blob:none --sparse https://github.com/DereckAn/minecraft
-cd minecraft
-git sparse-checkout set difficul_af
+git init 
+git remote add origin https://github.com/DereckAn/minecraft.git
+git config core.sparseCheckout true
+git sparse-checkout set mods
+git pull origin main 
+
+
 
 
 
@@ -52,68 +55,3 @@ if not (exists folder (path to home folder as text) & ".minecraft:versions:1.20.
 else
     display dialog "Forge found." buttons {"OK"} default button 1
 end if
-
-set links to {"https://github.com/DereckAn/minecraft/blob/main/difficul_af/BOMD-Forge-1.20.1-1.0.4.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/BiomesOPlenty-1.20.1-18.0.0.598.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/CreativeCore_FORGE_v2.11.11_mc1.20.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/Dungeon%20Crawl-1.20.1-2.3.14.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/DungeonsArise-1.20.1-2.1.57-release.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/EnhancedVisuals_FORGE_v1.6.9_mc1.20.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/FallingTree-1.20.1-4.3.2.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/HammersAndExcavators-1.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/HopoBetterMineshaft-%5B1.20-1.20.1%5D-1.1.8.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/HopoBetterRuinedPortals-%5B1.20-1.20.1%5D-1.3.6.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/L_Enders_Cataclysm-1.39%20-1.20.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/MoreBows-CJ-forge-mc1.20x-1.5.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/MutantMonsters-v8.0.6-1.20.1-Forge.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/OnlyHammers-1.20.1-0.5-Forge.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/Philips-Ruins1.20.1-2.7.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/PuzzlesLib-v8.1.11-1.20.1-Forge.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/TerraBlender-forge-1.20.1-3.0.0.169.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/Terralith_1.20.1_v2.4.5.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/TheHammerMod-1.20.1-beta3.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/Weeping-Angels-46.0.1-forge.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/Xaeros_Minimap_23.9.3_Forge_1.20.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/YungsApi-1.20-Forge-4.0.2.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/YungsBetterEndIsland-1.20-Forge-2.0.4.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/YungsBetterNetherFortresses-1.20-Forge-2.0.5.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/badpackets-forge-0.4.3.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/balm-forge-1.20.1-7.1.4.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/caelus-forge-3.1.0%2B1.20.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/citadel-2.4.9-1.20.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/cloth-config-11.1.106-forge.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/colytra-forge-6.2.2%2B1.20.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/connectedglass-1.1.9-forge-mc1.20.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/creeperoverhaul-3.0.1-forge.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/cupboard-1.20.1-2.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/dungeons-and-taverns-v2.1.4%20%5BForge%5D.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/explorations-forge-1.20.1-1.5.2.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/explorify-v1.3.0-mc1.20u1.20.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/fusion-1.1.0b-forge-mc1.20.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/geckolib-forge-1.20.1-4.3.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/gravestone-1.20.1-1.0.10.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/improvedmobs-1.20.1-1.11.6-forge.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/imst-2.1.0.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/jei-1.20.1-forge-15.2.0.27.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/majrusz-library-forge-1.20.1-7.0.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/majruszs-difficulty-forge-1.20.1-1.9.2.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/repurposed_structures-7.1.11%2B1.20.1-forge.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/resourcefulconfig-forge-1.20.1-2.1.0.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/spore_1.20.1_2.0.0b.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/structureessentials-1.20.1-3.2.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/supermartijn642corelib-1.1.16-forge-mc1.20.1.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/tenshilib-1.20.1-1.7.2-forge.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/u_team_core-forge-1.20.1-5.1.4.269.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/useful_backpacks-forge-1.20.1-2.0.1.122.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/waystones-forge-1.20-14.0.2.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/wthit-forge-8.4.3.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/ctov-3.3.6.jar",
-    "https://github.com/DereckAn/minecraft/blob/main/difficul_af/rare-ice-0.6.0.jar", 
-    https://github.com/DereckAn/minecraft/blob/main/difficul_af/TravelersBackpack-1.20.1-9.1.11.jar
-}
-
-repeat with link in links
-    set fileName to last item of paragraphs of link
-    set downloadPath to (path to home folder as text) & "Downloads:" & fileName
-    do shell script "curl -o " & quoted form of POSIX path of downloadPath & " " & quoted form of link
-end repeat
--- pruebas.sh
+++ pruebas.sh
@@ -6,7 +6,7 @@ if ! which brew >/dev/null; then
         echo "Homebrew found."
 fi
 
-# Verificar si SVN está instalado
+# Verificar si Git está instalado
 if ! command -v git &> /dev/null; then
     echo "Git no está instalado. Instalándolo..."
     brew install git
@@ -35,11 +35,69 @@ fi
 # Cambiar al directorio de descargas
 cd ~/Downloads
 
-mkdir mods
-cd mods
+# Verificar si existe la carpeta "mods" y eliminarla
+if [ -d "mods" ]; then
+    rm -rf mods
+fi
 
+# Crear una carpeta temporal
+mkdir temp
+cd temp
+
+# Descargando unicamente la carpeta mods del repositorio 
 git init 
 git remote add origin https://github.com/DereckAn/minecraft.git
 git config core.sparseCheckout true
-git sparse-checkout set mods
+git sparse-checkout  set mods
 git pull origin main 
+
+# Mover la carpeta "mods" a Downloads
+mv mods ../
+
+# Regresar a la carpeta Downloads y eliminar la carpeta temporal
+cd ..
+rm -rf temp
+
+# Definir la ruta y versión de Forge
+FORGE_VERSION="1.20.1-47.3.0"
+FORGE_INSTALLER="forge-${FORGE_VERSION}-installer.jar"
+
+# Determinar el sistema operativo y establecer la ruta de Minecraft Forge
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    MINECRAFT_FORGE_DIR="$HOME/Library/Application Support/minecraft/versions/${FORGE_VERSION}"
+    MINECRAFT_DIR="$HOME/Library/Application Support/minecraft"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux
+    MINECRAFT_FORGE_DIR="$HOME/.minecraft/versions/${FORGE_VERSION}"
+    MINECRAFT_DIR="$HOME/.minecraft"
+else
+    echo "Sistema operativo no soportado."
+    exit 1
+fi
+
+# Verificar si la versión específica de Forge ya está instalada
+if [ -d "${MINECRAFT_FORGE_DIR}" ]; then
+    echo "Minecraft Forge ${FORGE_VERSION} ya está instalado."
+else
+    echo "Minecraft Forge ${FORGE_VERSION} no está instalado. Procediendo con la instalación..."
+
+    # Cambiar al directorio de Minecraft
+    cd "${MINECRAFT_DIR}"
+
+    # Crear una carpeta para Minecraft Forge si no existe
+    mkdir -p "versions/${FORGE_VERSION}"
+    cd "versions/${FORGE_VERSION}"
+
+    # Descargar el archivo forge installer si no existe
+    if [ ! -f "${FORGE_INSTALLER}" ]; then
+        echo "Descargando Minecraft Forge ${FORGE_VERSION}..."
+        curl -O "https://maven.minecraftforge.net/net/minecraftforge/forge/${FORGE_VERSION}/${FORGE_INSTALLER}"
+    else
+        echo "El instalador de Minecraft Forge ${FORGE_VERSION} ya está descargado."
+    fi
+
+    # Ejecutar el instalador de Forge como cliente
+    echo "Instalando Minecraft Forge ${FORGE_VERSION}..."
+    java -jar "${FORGE_INSTALLER}" --installClient
+fi
